a2452304c2843ebb3a7df6158b63b69f6b502f87
Juan P. Romano <programming@jpromano.net>
2021-04-17 12:32:48 -0300

    Ejercicio 1.2

--- ejercicio_01_01.py
+++ ejercicio_01_01.py
@@ -2,11 +2,14 @@
 
 ## Declaro variables y solicito al usuario que ingrese un número entero
 numeroInt = input("Por favor, ingresa un numéro entero: ")
+print("")
 
 ## Convierto el valor a coma flotante
 numeroFloat = float(numeroInt)
 
 ## Imprimo por pantalla la version con float y el tipo de variable
+print("<===== Numero flotante y tipo de dato =====>")
+print("")
 print("Su número flotante es: ")
 print(numeroFloat)
 print("Y su tipo de variable es: ")
