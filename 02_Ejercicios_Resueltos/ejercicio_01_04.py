395ced26390e6fbd436ea960f98348fa6afdb973
Juan P. Romano <programming@jpromano.net>
2021-05-11 16:04:04 -0300

    Ejercicio 01-04

--- 02_Ejercicios_Resueltos/ejercicio_01_04.py
+++ 02_Ejercicios_Resueltos/ejercicio_01_04.py
@@ -1,5 +1,8 @@
-primerNumero = int(input('Ingresar el numero inicial: '))
-segundoNumero = int(input('Ingresar el numero final: '))
+# Declaro las variables y solicito al usuario que ingrese los numeros
+primerNumero = int(input('Ingresar el numero de inicio: '))
+segundoNumero = int(input('Ingresar el numero de fin: '))
+
+# Arranco un bucle while entre el primer y segundo numero hasta que resuelva la condicion.
 while primerNumero <= segundoNumero :
     if primerNumero%2 == 0 :
         print(primerNumero)
